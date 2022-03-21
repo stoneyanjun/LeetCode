0701061f2b18cd5a2b125ab98e992e1c2cf2f3e2
stoneyanjun <stoneyanjun@gmail.com>
2022-03-21 11:25:10 +0800

    init

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by stone on 2022/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
